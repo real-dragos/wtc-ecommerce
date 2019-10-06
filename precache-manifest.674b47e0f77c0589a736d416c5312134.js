3fe63751381c7cc0c3d2e8492474f25fdd08f9f1
real-dragos <dumitrescu.dragos.lucian87@gmail.com>
2019-10-06 20:02:43 +0300

    Updates

---
+++ precache-manifest.674b47e0f77c0589a736d416c5312134.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102e902d02c1be9a5497ac191ade75fd",
+    "url": "/wtc-ecommerce/index.html"
+  },
+  {
+    "revision": "d062e637b5215cdbb3dd",
+    "url": "/wtc-ecommerce/static/css/main.84782cdf.chunk.css"
+  },
+  {
+    "revision": "9ee1c3c02414952d026c",
+    "url": "/wtc-ecommerce/static/js/2.df331783.chunk.js"
+  },
+  {
+    "revision": "d062e637b5215cdbb3dd",
+    "url": "/wtc-ecommerce/static/js/main.2409e2a2.chunk.js"
+  },
+  {
+    "revision": "25c7d9aa8606dc0535e3",
+    "url": "/wtc-ecommerce/static/js/runtime-main.6a5163ac.js"
+  }
+]);
